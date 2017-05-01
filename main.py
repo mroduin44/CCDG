e1e248ef431df8be86780acb0f87589993a1e461
Michael Roduin <Michael.Roduin@Eastsidecatholicschool.org>
2017-05-01 12:58:16 -0700

    Basic game test functions added

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 HEIGHT = 730
 WIDTH = 1366
+Position=(0,0)
 
 alien = Actor('alien')
-alien.right = 0, 365 
+alien.topright = 0, 0 
 
 def draw():
     screen.clear()
     alien.draw()
 
 def update():
-    alien.left += 2
-    if alien.left > WIDTH:
-        alien.right = 0
+    print(Position[0])
     
 def on_mouse_down():
-    print("Mouse button clicked")
+    
 
 def on_mouse_down(pos):
-    print("Mouse button clicked at", pos)
+    
 
 def on_mouse_down(button):
-    print("Mouse button", button, "clicked")
+    
 
 def on_mouse_down(pos, button):
-    print("Mouse button", button, "clicked at", pos)
+    
+
+def on_mouse_move(pos):
+    Position=pos
